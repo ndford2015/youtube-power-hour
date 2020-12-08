2a9df54a7b90e189df198f5d33bb35f3ede63504
Nate Ford <ndford2015@gmail.com>
2020-12-08 12:05:41 -0600

    [ADD] heroku app initial commit

---
+++ src/App.tsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react';
+import YouTube from 'react-youtube';
+import './App.css';
+import { getPlaylistVideoIds, getVideos, VideoItem, videoItemToPlayerItem, VideoPlayerItem } from './utils';
+import { Button, Checkbox, Header, Icon, Input, Label, Loader, Message, Popup } from 'semantic-ui-react';
+import { SemanticICONS } from 'semantic-ui-react/dist/commonjs/generic';
+import { ERROR_TEXT } from './constants';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [videoPlayerItems, setVideoPlayerItems] = useState<VideoPlayerItem[]>([]);
+  const [videoIndex, setVideoIndex] = useState<number>(0);
+  const [showDrinkText, setShowDrinkText] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hideHeader, setHideHeader] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const hidden: string = hideHeader ? ' hidden' : ''
+  
+
+  const validateAndSetPlaylist = async (evt:any) => {
+    evt.preventDefault();
+    setError('');
+    setVideoIndex(0);
+    setVideoPlayerItems([]);
+    const playlistId: string = getPlaylistIdFromUrl(url);
+    if (!playlistId) {
+      setError(ERROR_TEXT.INVALID_URL);
+      return;
+    }
+    setIsLoading(true);
+    const videoIds: string[] = await getPlaylistVideoIds(playlistId);
+    const videos: VideoItem[] = await getVideos(videoIds);
+    if (videos.length < 60) {
+      setError(ERROR_TEXT.INVALID_PLAYLIST);
+      return;
+    }
+    const videoPlayerItems: VideoPlayerItem[] = videos.map(videoItemToPlayerItem);
+    setVideoPlayerItems(videoPlayerItems);
+    setIsLoading(false);
+  }
+
+  const getPlaylistIdFromUrl = (url: string) => {
+    const paramsStartIndex: number = url.indexOf('?');
+    if (paramsStartIndex === -1) {
+      console.log('invalid playlist url');
+      return '';
+    }
+    const queryParams: string = url.substring(paramsStartIndex, url.length);
+    const allParams: string[] = queryParams.split('&');
+    const playlistIdParams: string[] = allParams.filter((param:string) => param.includes('list'));
+    if (!playlistIdParams.length) {
+      console.log('invalid playlist url');
+      return '';
+    }
+    return playlistIdParams[0].split('=')[1];
+  }
+
+  const onReady = (evt) => {
+    evt.target.cueVideoById(videoPlayerItems[videoIndex]);
+    setHideHeader(true);
+    setVideoIndex(videoIndex + 1);
+  }
+
+  const displayDrinkText = () => {
+    setShowDrinkText(true);
+    setTimeout(() => setShowDrinkText(false), 2000);
+  }
+
+  const onPlayerStateChange = (evt) => {
+    if (videoIndex < 60) {
+      displayDrinkText();
+      evt.target.seekTo(0);
+      evt.target.loadVideoById(videoPlayerItems[videoIndex]);
+      setVideoIndex(videoIndex + 1);
+    }
+  }
+
+
+  const getVideoPlayer = (): JSX.Element | null => {
+    if (error) {
+      return <Message compact error content={error}  />
+    }
+    if (isLoading) {
+      return <Loader active />;
+    }
+    return videoPlayerItems.length === 0
+      ? null
+      : <YouTube 
+          onReady={onReady}
+          onEnd={onPlayerStateChange}
+          onError={onPlayerStateChange}
+          containerClassName="youtube-container"
+          opts={{playerVars: {controls: 0}, width: '90%', height: '90%'}}
+        />
+  }
+
+  const toggleHeader = () => {
+    setHideHeader(!hideHeader);
+  }
+
+
+  return (
+    <div className="App">
+      {videoIndex > 0 && 
+      <Label 
+        className="playlist-meta" 
+        content={`Video ${videoIndex} / 60`} 
+      />}
+      <Checkbox 
+        label="Hide Header" 
+        toggle checked={hideHeader} 
+        onClick={toggleHeader}
+        className="hide-header-toggle"
+      />
+      <div className="heading">
+        <Header size="medium" className={`title${hidden}`} textAlign="left">
+          <div>
+            <span>YouTube</span>
+            <span>Power</span>
+            <span>Hour</span>
+          </div>
+        </Header>
+      <div 
+        className="playlist-form"
+      >
+        <div className="form-header">
+          <Label 
+            className={hidden}
+            icon="arrow alternate circle down" 
+            size="big" 
+            content="Enter YouTube playlist URL" 
+          />
+        </div>
+        <Popup
+          trigger={
+            <Input type="text"
+              className={hidden}
+              value={url}
+              onChange={e => setUrl(e.target.value)} 
+              action={{
+                color: 'teal',
+                labelPosition: 'right',
+                icon: 'youtube',
+                content: 'Generate Power Hour',
+                onClick: validateAndSetPlaylist
+              }}
+            />
+          }
+          content="The playlist must contain at least 60 one-minute videos."
+        />
+      </div>
+      </div>
+      {showDrinkText 
+      && (<div className="drink-text">
+            <div>Drink!</div>
+          </div>)}
+      {getVideoPlayer()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils.ts
@@ -0,0 +1,154 @@
+import { API_KEY, YOUTUBE_API_BASE_URL } from "./constants";
+import {parse, toSeconds} from 'iso8601-duration';
+
+export enum API_ENDPOINT {
+  SEARCH = 'search',
+  PLAYLIST_ITEMS = 'playlistItems',
+  VIDEOS = 'videos'
+}
+
+export async function getResource(endpoint: API_ENDPOINT, params: QUERY_PARAMS): Promise<Response> {
+  const url: string = `${YOUTUBE_API_BASE_URL}${endpoint}?${paramsToQueryString(params)}&key=${API_KEY}`;
+  return fetch(url);
+}
+
+export async function getPlaylistVideoIds(playlistId: string) {
+  const params: PlaylistItemQueryParams = {
+    playlistId,
+    part: 'contentDetails',
+    maxResults: 50
+  }
+  const videoIds: string[] = [];
+  let response: Response;
+  let playlist: PlaylistItemResponse;
+  let pageToken: string = '';
+  while (pageToken !== undefined) {
+    response = await getResource(API_ENDPOINT.PLAYLIST_ITEMS, {
+      ...params, pageToken});
+    playlist = await response.json();
+    playlist.items.forEach((item: PlaylistItem) => {
+      videoIds.push(item.contentDetails.videoId);
+    });
+    pageToken = playlist.nextPageToken;
+  }
+  return videoIds;
+}
+
+export function getDurationSeconds(isoDuration: string) {
+  return toSeconds(parse(isoDuration));
+}
+
+export async function getVideos(ids: string[]): Promise<VideoItem[]> {
+  let videos: VideoItem[] = [];
+  for (let i: number = 0; i < ids.length; i+=50) {
+    const params: VideoQueryParams = {
+      part: 'contentDetails',
+      maxResults: 50,
+      id: ids.slice(i, i + 50).join(',')
+    }
+    const response: Response = await getResource(API_ENDPOINT.VIDEOS, params);
+    const videoResponse: VideosResponse = await response.json();
+    videos = videos.concat(videoResponse
+      .items
+      .filter(item => !isContentRestricted(item) 
+        && isUSAllowed(item)
+        && (getDurationSeconds(item.contentDetails.duration) > 60)));
+  }
+  return shuffleArray(videos);
+}
+
+export function isContentRestricted(item: VideoItem): boolean {
+  return item.contentDetails.contentRating.ytRating !== undefined;
+}
+
+export function isUSAllowed(item: VideoItem): boolean {
+  const { regionRestriction } = item.contentDetails
+  if (regionRestriction === undefined) {
+    return true;
+  }
+  if (regionRestriction.blocked !== undefined && regionRestriction.blocked.includes("US")) {
+    return false;
+  }
+  if (regionRestriction.allowed !== undefined && !regionRestriction.allowed.includes("US")) {
+    return false;
+  }
+  return true;
+}
+
+export function videoItemToPlayerItem(videoItem: VideoItem): VideoPlayerItem {
+  const {id, contentDetails } = videoItem;
+  const startSeconds: number = getStartTime(getDurationSeconds(contentDetails.duration));
+  return {
+    videoId: id,
+    startSeconds,
+    endSeconds: startSeconds + 60
+  }
+}
+
+function getStartTime(duration: number): number {
+  return Math.floor(Math.random() * Math.floor(duration - 60));
+}
+
+export function paramsToQueryString(params: any):string {
+  return Object.keys(params)
+      .map((key: any) => `${key}=${encodeURIComponent(params[key])}`)
+      .join('&');
+}
+
+function shuffleArray(array: any[]): any[] {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export interface CommonQueryParams {
+  readonly part: string;
+  readonly maxResults: number;
+}
+
+export interface SearchQueryParams extends CommonQueryParams {
+  readonly q: string;
+  readonly type: string;
+}
+
+export interface PlaylistItemQueryParams extends CommonQueryParams {
+  readonly playlistId: string;
+  readonly pageToken?: string;
+}
+
+export interface VideoQueryParams extends CommonQueryParams {
+  // comma delimited string of ids
+  readonly id: string;
+}
+
+export interface PlaylistItemResponse {
+  readonly items: PlaylistItem[];
+  readonly nextPageToken: string;
+}
+
+export interface PlaylistItem {
+  readonly contentDetails: {videoId: string};
+}
+
+export interface VideoItem {
+  readonly id: string,
+  readonly contentDetails: { 
+    duration: string, 
+    contentRating: {ytRating?: string},
+    regionRestriction?: {allowed?: string[], blocked?: string[]}  
+  }
+}
+
+export interface VideoPlayerItem {
+  readonly videoId: string,
+  readonly startSeconds: number,
+  readonly endSeconds: number
+}
+
+export interface VideosResponse {
+  readonly items: VideoItem[];
+}
+
+export type QUERY_PARAMS = Partial<SearchQueryParams & PlaylistItemQueryParams>;
