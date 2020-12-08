74678c68f2e57c7544b891fe916b79c811b5902e
Nate Ford <ndford2015@gmail.com>
2020-12-07 19:32:36 -0600

    [ADD] initial commit

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const API_KEY: string = 'AIzaSyCmJ_BMQrMhwqKfGXzExvW1gNax_dkxvNk';
+export const YOUTUBE_API_BASE_URL: string = 'https://youtube.googleapis.com/youtube/v3/';
+
+export enum ERROR_TEXT {
+    INVALID_URL = "The url you entered doesn't seem to be a valid YouTube playlist.",
+    INVALID_PLAYLIST = "This playlist doesn't have at least 60 one-minute videos."
+}
